d6016ce5eb540f25aa9f702413aed1361f51de18
tetras256 <tetras256@gmail.com>
2025-07-06 18:38:12 +0900

    初回コミット：サイト構成とHTML追加

---
+++ 【練習風景】全動画一覧/練習風景動画はここ.js
@@ -0,0 +1,15 @@
+//上から新しい動画のURL張ってね。ファイル追加日6/28ゆき
+//途中の部分をembedに書き換えてね
+// 練習風景動画はここ.js
+const videoList = [
+  "https://www.youtube.com/embed/dQw4w9WgXcQ",
+  "https://www.youtube.com/embed/2Vv-BfVoq4g",
+  "https://www.youtube.com/embed/kXYiU_JCYtU",
+  "https://www.youtube.com/embed/CevxZvSJLk8",
+  "https://www.youtube.com/embed/ktvTqknDobU",
+  "https://www.youtube.com/embed/3JZ_D3ELwOQ",
+  "https://www.youtube.com/embed/fJ9rUzIMcZQ",
+  "https://www.youtube.com/embed/L_jWHffIx5E",
+  "https://www.youtube.com/embed/o1tj2zJ2Wvg",
+  "https://www.youtube.com/embed/s7jXASBWwwI"
+];
