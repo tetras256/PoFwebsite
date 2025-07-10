8fe28eb60f052719c018b605d9247b1f422169cb
tetras256 <tetras256@gmail.com>
2025-07-11 00:53:06 +0900

    えっくす　相手が嫌がることが至高

--- indexscript.js
+++ indexscript.js
@@ -1,4 +1,4 @@
- document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
     const sliders = document.querySelectorAll('.slider');
 
     sliders.forEach((slider) => {
